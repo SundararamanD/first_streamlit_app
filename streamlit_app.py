6c040a06330c5344267236a371e07eade35f1621
SundararamanD <139410651+SundararamanD@users.noreply.github.com>
2023-07-12 21:02:59 -0400

    Update streamlit_app.py
    added images to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('BreakFast Menu')
-streamlit.text('Omega3 & Blueberry Oat meal')
-streamlit.text('PanCake & Egg Omlette')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega3 & Blueberry Oat meal')
+streamlit.text('🥑🍞 PanCake & Egg Omlette')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
