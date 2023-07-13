e95e1c7982d1b7c7f2bcfe845a851717a57f0125
SundararamanD <139410651+SundararamanD@users.noreply.github.com>
2023-07-12 21:06:58 -0400

    Update streamlit_app.py
    try backlash to add '

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Moms New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('BreakFast Favorites')
 streamlit.text('🥣 Omega3 & Blueberry Oat meal')
 streamlit.text('🥑🍞 PanCake & Egg Omlette')
