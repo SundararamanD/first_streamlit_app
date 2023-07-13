3538d72afda2956f343cdf913f22832c16aac5d1
SundararamanD <139410651+SundararamanD@users.noreply.github.com>
2023-07-12 20:56:14 -0400

    Update streamlit_app.py
    updated line 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('BreakFast Menu')
+streamlit.body('BreakFast Menu')
 
 streamlit.title('Omega3 & Blueberry Oat meal')
